4dbd71ef56dc4e1fb15d783356d448e387e0cf13
stephaniemlozano <stephaniesadowski@Stephanies-Air.home>
2022-10-22 22:54:24 -0400

    added placeholder to login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,36 +1,48 @@
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
   const [loginForm, setLoginForm] = useState({})
   const navigate = useNavigate()
-  
+
   const formSubmit = (event) => {
     event.preventDefault()
     fetch(`${process.env.REACT_APP_API_ENDPOINT}/login`, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(loginForm),
     })
-    .then(response => response.json())
-    .then(data => {
-    localStorage.setItem('userToken', data.accessToken)
-      navigate('/admin')
-    })
-    .catch(error => console.error(error))
+      .then((response) => response.json())
+      .then((data) => {
+        localStorage.setItem('userToken', data.accessToken)
+        navigate('/admin')
+      })
+      .catch((error) => console.error(error))
   }
-  
+
   return (
-    <div className="login-form">
-      <form action="submit" onSubmit={formSubmit} method='post'>
+    <div className='login-form'>
+      <form action='submit' onSubmit={formSubmit} method='post'>
         <h2>Log In</h2>
-        <label htmlFor="">Email:</label>
-        <input type="email" onChange={event => setLoginForm({...loginForm, email: event.target.value})} />
-        <label htmlFor="">Password:</label>
-        <input type="password" onChange={event => setLoginForm({...loginForm, password: event.target.value})} />
-        <button type="submit">Log In</button>
+        <label htmlFor=''>Email:</label>
+        <input
+          type='email'
+          placeholder='ex: you@email.com'
+          onChange={(event) =>
+            setLoginForm({ ...loginForm, email: event.target.value })
+          }
+        />
+        <label htmlFor=''>Password:</label>
+        <input
+          type='password'
+          placeholder='ex: supersecretpw22'
+          onChange={(event) =>
+            setLoginForm({ ...loginForm, password: event.target.value })
+          }
+        />
+        <button type='submit'>Log In</button>
       </form>
     </div>
   )
